b2ed1e9f929098b5f5a1695c75d40d32e15df693
hithinker <ayahui3@126.com>
2018-04-16 19:20:58 +0800

    C8

---
+++ Aesthetics/initLesson.R
@@ -0,0 +1,7 @@
+# Code placed in this file fill be executed every time the
+      # lesson is started. Any variables created here will show up in
+      # the user's working directory and thus be accessible to them
+      # throughout the lesson
+my_color <- "#4ABEFF"
+val = c("#E41A1C", "#377EB8")
+lab = c("Manual", "Automatic")
